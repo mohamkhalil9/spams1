bd2f48eb60de746c613c28d57ba97034244ef29d
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-30 21:46:25 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("561611876769726476")
+setInterval(function() {
+channel.send(`Mixwell Community by : @!                Mohamad_Doleh .#6334 Mixwell Community by : @!                Mohamad_Doleh .#6334`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
