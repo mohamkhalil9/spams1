0ea594f5316d700545eeeb00840cb43ff1e8ad1b
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-31 21:54:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("561619609510215683")
 setInterval(function() {
-channel.send(`Mixwell Community by : @!                Mohamad_Doleh .#6334 Mixwell Community by : @!                Mohamad_Doleh .#6334`);
+channel.send(`@here The Best Spamer @!                Mohamad_Doleh .#6334`);
 }, 30)
 })
 
