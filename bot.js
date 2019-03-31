9426208ab8437fff211373a0d6f6de3e1f45486d
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-31 22:01:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("561619609510215683")
 setInterval(function() {
-channel.send(`@here The Best Spamer @!                Mohamad_Doleh .#6334`);
+channel.send(` The Best Spamer @!                Mohamad_Doleh .#6334 ``` مال ربكو ينعن الله ````);
 }, 30)
 })
 
